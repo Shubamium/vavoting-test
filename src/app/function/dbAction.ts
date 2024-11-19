e1001dfe13cb064a4dc5acb3561f011fa40d3bea
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-11-19 21:47:47 +0700

    Fix more connection issue

--- src/app/function/dbAction.ts
+++ src/app/function/dbAction.ts
@@ -2,13 +2,15 @@ import createConnection from "./db";
 
 export async function saveCode(email: string, code: string) {
   const connection = await createConnection();
-
-  const res = await connection.execute(
-    `INSERT INTO verification (code,user_email,created,is_verified) VALUES (?,?,CURRENT_TIMESTAMP,?)`,
-    [code, email, false]
-  );
-  await connection.end();
-  console.log(res, "adding code");
+  try {
+    const res = await connection.execute(
+      `INSERT INTO verification (code,user_email,created,is_verified) VALUES (?,?,CURRENT_TIMESTAMP,?)`,
+      [code, email, false]
+    );
+    console.log(res, "adding code");
+  } finally {
+    await connection.end();
+  }
 }
 
 export async function verifyCode(email: string, code: string) {
@@ -16,13 +18,16 @@ export async function verifyCode(email: string, code: string) {
 	SELECT * FROM verification WHERE user_email = ? AND code = ? AND created >= NOW() - INTERVAL 5 MINUTE;
 `;
   const connection = await createConnection();
-  const param = [email, code];
-  const res = await connection.query(query, param);
-  const rows = res[0] as unknown;
+  try {
+    const param = [email, code];
+    const res = await connection.query(query, param);
+    const rows = res[0] as unknown;
 
-  const data = rows as Array<any>;
-  await connection.end();
-  return data.length === 1;
+    const data = rows as Array<any>;
+    return data.length === 1;
+  } finally {
+    await connection.end();
+  }
 }
 
 export async function saveVote(
@@ -31,33 +36,37 @@ export async function saveVote(
   vote_option: string
 ) {
   const connection = await createConnection();
-  const res = await connection.execute(
-    `
-		INSERT INTO votes (poll_id,user_email,timestamp,vote_option) VALUES (?,?,CURRENT_TIMESTAMP,?)`,
-    [poll_id, user_email, vote_option]
-  );
-
-  await connection.end();
-  return res;
+  try {
+    const res = await connection.execute(
+      `
+			INSERT INTO votes (poll_id,user_email,timestamp,vote_option) VALUES (?,?,CURRENT_TIMESTAMP,?)`,
+      [poll_id, user_email, vote_option]
+    );
+    return res;
+  } finally {
+    await connection.end();
+  }
 }
 
 export async function getVote(poll_id: string, until: number) {
   if (until === -1) return [];
   const connection = await createConnection();
-  let tally = [];
-  for (let i = 0; i < until; i++) {
-    const res = (await connection.execute(
-      `
-			SELECT COUNT(*) as count FROM votes WHERE poll_id = ? AND vote_option = ?;
-			`,
-      [poll_id, i]
-    )) as any;
+  try {
+    let tally = [];
+    for (let i = 0; i < until; i++) {
+      const res = (await connection.execute(
+        `
+				SELECT COUNT(*) as count FROM votes WHERE poll_id = ? AND vote_option = ?;
+				`,
+        [poll_id, i]
+      )) as any;
 
-    const count = res[0][0].count;
-    console.log(res[0][0].count);
-    tally.push(count);
+      const count = res[0][0].count;
+      console.log(res[0][0].count);
+      tally.push(count);
+    }
+    return tally;
+  } finally {
+    await connection.end();
   }
-
-  await connection.end();
-  return tally;
 }
